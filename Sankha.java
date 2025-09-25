ccc49702f0075a5f61e5bac2206b0da7f50a80fc
Sankhadeep Bera <berasankhadeep20@gmail.com>
2025-09-25 12:38:45 +0530

    Rectangular matrix

--- Sankha.java
+++ Sankha.java
@@ -4,13 +4,15 @@ public class Sankha
     public static void main(String[] args) 
     {
         Scanner se = new Scanner(System.in);
-        System.out.println("Enter the size of the matrix");
+        System.out.println("Enter the number of rows of the matrix");
         int n = se.nextInt();
-        int a[][] = new int[n][n];
+        System.out.println("Enter the number of columns of the matrix");
+        int m = se.nextInt();
+        int a[][] = new int[n][m];
         System.out.println("Enter the elements of the matrix");
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
                 a[i][j] = se.nextInt();
             }
@@ -18,7 +20,7 @@ public class Sankha
         System.out.println("The matrix is:");
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
                 System.out.print(a[i][j]+" ");
             }
